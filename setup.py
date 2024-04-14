7750e5152d785f78f82a66071ff26fadaca2b8ec
xandys <154770152+xandys@users.noreply.github.com>
2024-04-14 18:26:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='jablotron2mqtt',
       version='0.1',
       description='Bridge for interfacing jablotron 6x alarms via MQTT',
-      url='https://github.com/pezinek/py-jablotron6x',
+      url='https://github.com/xandys/py-jablotron6x',
       packages=['jablotron2mqtt'],
       install_requires=[
         'jablotron',
